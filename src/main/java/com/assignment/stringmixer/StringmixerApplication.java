f686cbc2051434c8109fc9f746d1fe1cec61a792
Kasun Madushanka <kmadushanka40@gmail.com>
2021-11-02 09:26:08 +0000

    first

---
+++ src/main/java/com/assignment/stringmixer/StringmixerApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.stringmixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringmixerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringmixerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/stringmixer/StringmixerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.stringmixer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringmixerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
